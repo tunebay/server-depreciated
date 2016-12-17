a02e8458cfa85422ed1f3fa42a6a5212cc9accea
Mali Michael <malimccalla@gmail.com>
2016-12-17 23:40:37 +0000

    install mocha and chai

---
+++ test/user_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const User = require('../models/user');
+
+describe('User', () => {
+  it('Runs tests', (done) => {
+    expect(1).to.equal(1);
+    done();
+  });
+});
