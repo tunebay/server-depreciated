b78cd3340ab9beea80cdef3a848eb4abf02efc8e
Mali Michael <malimccalla@gmail.com>
2016-12-18 13:09:06 +0000

    auth controller export function seperately

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,39 +1,37 @@
 const User = require('../models/user');
 
-module.exports = {
-  signup(req, res, next) {
-    const user = new User(req.body);
+exports.signup = (req, res, next) => {
+  const user = new User(req.body);
 
-    User.findByUsername(req.body.username)
-      .then((foundUsername) => {
-        if (foundUsername) {
-          return res.status(422)
-            .json({
-              error: `The username ${user.username} is not available.`
-            });
-        }
-        return User.findByEmail(req.body.email)
-          .then((foundEmail) => {
-            if (foundEmail) {
-              return res.status(422)
-                .json({
-                  error: 'This email is already in use.'
-                });
-            }
-            return user.save()
-              .then((userRecord) => {
-                res.status(200)
-                  .json({
-                    status: 'success',
-                    message: 'Successfully created user.',
-                    token: `${userRecord.id}`
-                  });
-              });
+  User.findByUsername(req.body.username)
+    .then((foundUsername) => {
+      if (foundUsername) {
+        return res.status(422)
+          .json({
+            error: `The username ${user.username} is not available.`
           });
-      })
-      .catch((error) => {
-        console.log('*** SIGNUP ERROR **** ', error);
-        return next(error);
-      });
-  }
+      }
+      return User.findByEmail(req.body.email)
+        .then((foundEmail) => {
+          if (foundEmail) {
+            return res.status(422)
+              .json({
+                error: 'This email is already in use.'
+              });
+          }
+          return user.save()
+            .then((userRecord) => {
+              res.status(200)
+                .json({
+                  status: 'success',
+                  message: 'Successfully created user.',
+                  token: `${userRecord.id}`
+                });
+            });
+        });
+    })
+    .catch((error) => {
+      console.log('*** SIGNUP ERROR **** ', error);
+      return next(error);
+    });
 };
