a5e95e10aa2768d6104341a29077548ed55363d6
Mali Michael <malimccalla@gmail.com>
2016-12-18 17:56:22 +0000

    Stub out couple of tests to write later

--- test/controllers/authentication_test.js
+++ test/controllers/authentication_test.js
@@ -4,19 +4,29 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Authentication controller', () => {
-  it('POST to /signup creates new user', (done) => {
-    request(app)
-      .post('/signup')
-      .send({
-        displayName: 'Mali Michael',
-        username: 'malimichael',
-        email: 'mali@tunebay.com',
-        password: 'password',
-        accountType: 'artist'
-      })
-      .end((err, res) => {
-        expect(res.body.message).to.equal('Successfully created a user.');
-        done();
-      });
+  describe('POST /signup', () => {
+    it('Creates new user', (done) => {
+      request(app)
+        .post('/signup')
+        .send({
+          displayName: 'Mali Michael',
+          username: 'malimichael',
+          email: 'mali@tunebay.com',
+          password: 'password',
+          accountType: 'artist'
+        })
+        .end((err, res) => {
+          expect(res.body.message).to.equal('Successfully created a user.');
+          done();
+        });
+    });
+
+    xit('responds with an error if email is in use', (done) => {
+      done();
+    });
+
+    xit('reponds with an error if username is in use', (done) => {
+      done();
+    });
   });
 });
