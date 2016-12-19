dda1f0e670925847e0de8054a3dfe7fed560f971
Mali Michael <malimccalla@gmail.com>
2016-12-19 10:00:03 +0000

    Adds requireAuth middleware to router

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
+require('./services/passport');
+
+const passport = require('passport');
 const Authentication = require('./controllers/authentication');
 
+const requireAuth = passport.authenticate('jwt', { session: false });
+
 module.exports = (app) => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ message: 'You are authorized' });
+  });
   app.post('/signup', Authentication.signup);
 };
