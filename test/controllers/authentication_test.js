eb8090312e3c07e370e1c74484d1285cd75d7e2f
Mali Michael <malimccalla@gmail.com>
2016-12-19 09:13:48 +0000

    testing for error on duplicate email or username POST

--- test/controllers/authentication_test.js
+++ test/controllers/authentication_test.js
@@ -21,12 +21,58 @@ describe('Authentication controller', () => {
         });
     });
 
-    xit('responds with an error if email is in use', (done) => {
-      done();
+    it('responds with an error if email is in use', (done) => {
+      request(app)
+        .post('/signup')
+        .send({
+          displayName: 'Mali Michael',
+          username: 'malimichael',
+          email: 'mali@tunebay.com',
+          password: 'password',
+          accountType: 'artist'
+        })
+        .end(() => {
+          request(app)
+            .post('/signup')
+            .send({
+              displayName: 'Imposter Michael',
+              username: 'malimichael',
+              email: 'imposter@tunebay.com',
+              password: 'password',
+              accountType: 'artist'
+            })
+            .end((err, res) => {
+              expect(res.body.error).to.equal('The username malimichael is not available.');
+              done();
+            });
+        });
     });
 
-    xit('reponds with an error if username is in use', (done) => {
-      done();
+    it('responds with an error if email is in use', (done) => {
+      request(app)
+        .post('/signup')
+        .send({
+          displayName: 'Mali Michael',
+          username: 'malimichael',
+          email: 'mali@tunebay.com',
+          password: 'password',
+          accountType: 'artist'
+        })
+        .end(() => {
+          request(app)
+            .post('/signup')
+            .send({
+              displayName: 'Imposter Michael',
+              username: 'imposter',
+              email: 'mali@tunebay.com',
+              password: 'password',
+              accountType: 'artist'
+            })
+            .end((err, res) => {
+              expect(res.body.error).to.equal('This email is already in use.');
+              done();
+            });
+        });
     });
   });
 });
