52001776f2c908d42491ad0145cb48896d7acd07
Mali Michael <malimccalla@gmail.com>
2016-12-20 14:32:43 +0000

    Match username route

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 require('./services/passport');
-
 const passport = require('passport');
-const Authentication = require('./controllers/authentication');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
+
+const Authentication = require('./controllers/authentication');
+const Profile = require('./controllers/profile');
+
 
 module.exports = (app) => {
   app.get('/', requireAuth, (req, res) => {
@@ -12,4 +14,5 @@ module.exports = (app) => {
   });
   app.post('/login', requireLogin, Authentication.login);
   app.post('/signup', Authentication.signup);
+  app.get('/:username', Profile.loadUser);
 };
