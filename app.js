2133c4099760c40ad51392892c355d23ff553dfe
Mali Michael <malimccalla@gmail.com>
2016-12-24 14:38:08 +0000

    Remove logging from test env

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const router = require('./router');
 const app = express();
 
 // App set up
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('combined'));
+}
 
-app.use(morgan('combined'));
 app.use(bodyParser.json());
 router(app);
 
