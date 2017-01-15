016b93c125ddcaca37e85e430508cd53cf965063
Mali Michael <malimccalla@gmail.com>
2017-01-15 11:32:17 +0000

    Enable cors to allow access from 8080

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./router');
 
@@ -10,6 +11,12 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('combined'));
 }
 
+const corsConfig = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsConfig));
 app.use(bodyParser.json());
 router(app);
 
